315f86fcd57e1dc095ae569297ff83f02819e328
Steven Molina <steven.molina.slayer@gmail.com>
2024-05-18 21:42:10 -0300

    Created structure for project; Added v1 of Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-// import DisplayCard from "@/components/DisplayCard";
-import DisplayUser from "@/app/client-users/DisplayUserClient";
+"use client"
 
+import Banner from "@/components/layout/Banner";
+
+// import { Button } from "@/components/ui/button";
+// import { Spacer } from "@nextui-org/react";
+// import { Button as ButtonUI } from "@nextui-org/react";
 
 export default function Home() {
+  const bgHeroImage: string =
+    "https://images.unsplash.com/photo-1541701494587-cb58502866ab?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="">
       {/* <DisplayCard /> */}
-      <DisplayUser />
+      {/* <Button>Shadcn Button</Button>
+      <Spacer y={4} />
+      <ButtonUI>NextUI Button</ButtonUI> */}
+      <Banner
+      bgImage={bgHeroImage}
+      titleText="MOPERCLUB"
+      subtitleText="Experiencias Inolvidables"
+    />
     </main>
   );
 }
---
+++ src/components/layout/Banner.tsx
@@ -0,0 +1,36 @@
+// styles
+import Navbar from "./Navbar";
+
+// banner components types definition
+interface Props {
+  bgImage: string;
+  titleText: string;
+  subtitleText: string;
+}
+
+export default function Banner({ bgImage, titleText, subtitleText }: Props) {
+  // Banner Hero background image
+  const bgImageStyle = {
+    backgroundImage: `url(${bgImage})`
+  };
+  // returned component
+  return (
+    <>
+      {/* Home/Banner section wrapper*/}
+      <section className="home-page h-screen" style={bgImageStyle}>
+        <div className="banner__img-overlay w-full h-screen absolute bg-black/60"></div>
+        {/* opacity img */}
+        {/* Navbar component: (for it to be transparent and show bg image) */}
+        <Navbar />
+        {/* Baner centered elements/ Call to action */}
+        <div className="fluid-container banner flex justify-center items-center w-full h-screen ">
+          <div className="banner__border"></div>
+          <div className="text__container banner__text z-10 -translate-y-12">
+            <h2 className="banner__h2 tracking-tight text-8xl text-white">{titleText}</h2>
+            <h4 className="banner__h4 text-center text-4xl text-white">{subtitleText}</h4>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
