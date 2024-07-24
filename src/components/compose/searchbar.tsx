316fe6167405e132d04e128fd47dcea444b1ddb1
steven <stevenmolina2205@gmail.com>
2024-07-24 12:18:08 -0300

    Organizes files

---
+++ src/components/compose/searchbar.tsx
@@ -0,0 +1,38 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+export function SearchBar() {
+  // function
+  const handleSearch = () => {
+    // -- getting element and its input
+    const searchInput = document.getElementById("searchBarInput");
+    // null type check
+    if (searchInput instanceof HTMLInputElement) {
+      const searchValue = searchInput.value;
+      console.log(searchValue);
+    }
+  };
+  const showSearch = () => {
+    const searchInput = document.getElementById("searchBarInput");
+    // null type check
+    if (searchInput instanceof HTMLElement) {
+      searchInput.classList.toggle("hidden");
+    }
+  };
+  // return value
+  return (
+    <div className="flex items-center gap-2">
+      <input
+        onInput={handleSearch}
+        id="searchBarInput"
+        className="hidden rounded-2xl p-2 text-sm text-gray-900"
+        type="text"
+        placeholder="Search.."
+      />
+      <button onClick={showSearch} id="searchButton">
+        <MagnifyingGlassIcon className=" h-6 w-6 text-white" />
+      </button>
+      {/* <div>The thing you searched:</div>
+      <div id="searchContent"></div> */}
+    </div>
+  );
+}
